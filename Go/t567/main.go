2a148b19ccafa321a6f5d76a145c651e9c03261d
Yilin Fang <qzfyl98@outlook.com>
2021-02-10 10:24:20 +0800

    Go:finished t567

---
+++ Go/t567/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func checkInclusion(s1, s2 string) bool {
+	n, m := len(s1), len(s2)
+	if n > m {
+		return false
+	}
+	cnt := [26]int{}
+	for _, ch := range s1 {
+		cnt[ch-'a']--
+	}
+	left := 0
+	for right, ch := range s2 {
+		x := ch - 'a'
+		cnt[x]++
+		for cnt[x] > 0 {
+			cnt[s2[left]-'a']--
+			left++
+		}
+		if right-left+1 == n {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(checkInclusion("ab", "eidbaooo"))
+	fmt.Println(checkInclusion("ab", "eidboaoo"))
+}
