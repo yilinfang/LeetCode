3d63bae781b821b414be1026b2c0417be736edf5
Yilin Fang <qzfyl98@outlook.com>
2021-02-23 18:23:46 +0800

    Go:finished t697

--- Go/t697/main.go
+++ Go/t697/main.go
@@ -3,8 +3,40 @@ package main
 import "fmt"
 
 func findShortestSubArray(nums []int) int {
+	d := make(map[int]int)
+	fp := make(map[int]int)
+	lp := make(map[int]int)
+	for i, v := range nums {
+		value, ok := d[v]
+		if ok {
+			d[v] = value + 1
+			lp[v] = i
+		} else {
+			d[v] = 1
+			fp[v] = i
+			lp[v] = i
+		}
 
-	return 0
+	}
+	check := make([]int, 0)
+	max := 0
+	for k, v := range d {
+		if v > max {
+			max = v
+			check = check[0:0]
+			check = append(check, k)
+		} else if v == max {
+			check = append(check, k)
+		}
+	}
+	res := len(nums)
+	for _, v := range check {
+		r := lp[v] - fp[v] + 1
+		if r < res {
+			res = r
+		}
+	}
+	return res
 }
 
 func main() {
