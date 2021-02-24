476fb48f34b18d3e140cb03ce08df2b62b0c4d8e
Yilin Fang <qzfyl98@outlook.com>
2021-02-24 16:26:27 +0800

    Go:finished t766

---
+++ Go/t766/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	m, n := len(matrix), len(matrix[0])
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i-1][j-1] != matrix[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isToeplitzMatrix([][]int{{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}}))
+	fmt.Println(isToeplitzMatrix([][]int{{1, 2}, {2, 2}}))
+}
