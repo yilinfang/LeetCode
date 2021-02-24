a807e63f6a38fccd5711907c5fa1d5c53157fb71
Yilin Fang <qzfyl98@outlook.com>
2021-02-24 16:28:23 +0800

    Go:finished t766

--- Go/t766/main.go
+++ Go/t766/main.go
@@ -6,7 +6,7 @@ func isToeplitzMatrix(matrix [][]int) bool {
 	m, n := len(matrix), len(matrix[0])
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
-			if matrix[i-1][j-1] != matrix[i][j] {
+			if matrix[i][j] != matrix[i-1][j-1] {
 				return false
 			}
 		}
